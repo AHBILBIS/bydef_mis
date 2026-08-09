ec5236a57506840595078e0c27e14abbca9bf6bd
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-09 22:56:44 +0800

    Add is_mandatory field to PaymentCategory model

--- apps/finance/models.py
+++ apps/finance/models.py
@@ -5,6 +5,7 @@ class PaymentCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     default_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0.00)
+    is_mandatory = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "Payment Categories"
