73667166f7da0c4d405d118b0c88c09d6d7e9e73
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-09 19:06:51 +0800

    Update ALLOWED_HOSTS for Render domain

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,7 @@ if os.path.exists(env_file):
 
 SECRET_KEY = env('SECRET_KEY', default='fallback-insecure-key-for-dev')
 DEBUG = env('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
-
+ALLOWED_HOSTS = ['bydef-mis.onrender.com', '.onrender.com', '127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.admin',
